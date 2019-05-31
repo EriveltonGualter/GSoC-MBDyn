a4cea5b1b207ee556adb61301305a3ee32329b45
Erivelton <erivelton.gualter@gmail.com>
2019-05-31 15:39:49 -0400

    Added Beam Scripts

---
+++ Beam Scripts/cleanfolder.sh
@@ -0,0 +1,13 @@
+find . -type f -iname \*.mov -delete
+find . -type f -iname \*.out -delete
+find . -type f -iname \*.log -delete
+find . -type f -iname \*.ine -delete
+
+find . -type f -iname \*.abs -delete
+find . -type f -iname \*.act -delete
+find . -type f -iname \*.frc -delete
+find . -type f -iname \*.gen -delete
+find . -type f -iname \*.jnt -delete
+
+
+
